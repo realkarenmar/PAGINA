6bc34278258d59927abc960dba407fb860ee200a
Prestamo Estudiantes <prestamour@urosario.edu.co>
2019-09-25 16:59:47 -0500

    nuevos

---
+++ script1.js
@@ -0,0 +1,25 @@
+//console.log("hola aprendices desde el archivo");
+//var x = -15;
+//x >= 0 ? console.log(x) : console.log(-x);
+
+{
+    x = Math.PI;
+    cx = Math.cos(x);
+    console.log('cx', cx);
+}
+username = undefined
+if(username==null){
+    username = "Onomishu";
+}
+var text;
+var x = "10";
+switch (x) {
+    case 10:
+        text = "decena";
+        break;
+        default:
+        text = "sin valor";
+}
+console.log(text);
+
+
